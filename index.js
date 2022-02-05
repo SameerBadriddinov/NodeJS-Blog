7d96fbbaa8c0a54f45397da7246be050b524893c
SameerBadriddinov <99094663+SameerBadriddinov@users.noreply.github.com>
2022-02-05 23:31:39 +0200

    add env file

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const storePostMiddleware = require("./middleware/storePost");
 const authMiddleware = require("./middleware/auth");
 const redirectIfAuth = require("./middleware/redirect");
 
-const MongoUrl = "mongodb+srv://samar:zozmS9hwVNIpdsGr@cluster0.bbfkm.mongodb.net/node-blog"
+const MongoUrl = process.env.MongoUrl
 
 mongoose.connect(MongoUrl);
 
